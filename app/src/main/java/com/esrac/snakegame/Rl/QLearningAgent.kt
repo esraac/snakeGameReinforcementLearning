008ba45ed295849ba253910014d742004094219b
esraac <ecayirpunar@gmail.com>
2025-11-25 09:04:35 +0300

    learning agent eklendi

--- app/src/main/java/com/esrac/snakegame/Rl/QLearningAgent.kt
+++ app/src/main/java/com/esrac/snakegame/Rl/QLearningAgent.kt
@@ -1,2 +1,62 @@
 package com.esrac.snakegame.Rl
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.esrac.snakegame.Game.Direction
+import com.esrac.snakegame.Game.SnakeGame
+import kotlin.random.Random
+
+class QLearningAgent(
+    private val actions: List<Direction>,
+    private val alpha: Double = 0.1,
+    private val gamma: Double = 0.9,
+    private val epsilon: Double = 0.1
+) {
+    private val qTable = mutableMapOf<String, MutableMap<Direction, Double>>()
+
+    private fun chooseAction(state: String): Direction {
+        val stateActions = qTable.getOrPut(state) { mutableMapOf() }
+        return if (Random.nextDouble() < epsilon) actions.random()
+        else stateActions.maxByOrNull { it.value }?.key ?: actions.random()
+    }
+
+    fun updateQ(state: String, action: Direction, reward: Double, nextState: String) {
+        val nextActions = qTable.getOrPut(nextState) { mutableMapOf() }
+        val maxNextQ = nextActions.values.maxOrNull() ?: 0.0
+        val qValues = qTable.getOrPut(state) { mutableMapOf() }
+        val oldQ = qValues.getOrDefault(action, 0.0)
+        val newQ = oldQ + alpha * (reward + gamma * maxNextQ - oldQ)
+        qValues[action] = newQ
+    }
+
+    @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    fun train(episodes: Int = 1000, maxSteps: Int = 200): List<Int> {
+        val game = SnakeGame()
+        val scores = mutableListOf<Int>()
+
+        repeat(episodes) {
+            game.reset()
+            var totalReward = 0.0
+            var steps = 0
+
+            while (!game.isGameOver && steps < maxSteps) {
+                val state = game.getState().toString()
+                val action = chooseAction(state)
+                val reward = game.performStep(action)
+                val nextState = game.getState().toString()
+                updateQ(state, action, reward, nextState)
+                totalReward += reward
+                steps++
+            }
+
+            scores.add(game.score)
+            println("Episode ${it + 1}: Score = ${game.score}, TotalReward = ${"%.2f".format(totalReward)}")
+        }
+        return scores
+    }
+
+    fun getBestAction(state: String): Direction {
+        val stateActions = qTable.getOrPut(state) { mutableMapOf() }
+        return stateActions.maxByOrNull { it.value }?.key ?: actions.random()
+    }
+}
--- app/src/main/java/com/esrac/snakegame/Rl/State.kt
+++ app/src/main/java/com/esrac/snakegame/Rl/State.kt
@@ -1,2 +1,12 @@
 package com.esrac.snakegame.Rl
 
+data class State(
+    val headX: Int,
+    val headY: Int,
+    val foodDirX: Int,
+    val foodDirY: Int
+) {
+    override fun toString(): String {
+        return "${headX}_${headY}_${foodDirX}_${foodDirY}"
+    }
+}
